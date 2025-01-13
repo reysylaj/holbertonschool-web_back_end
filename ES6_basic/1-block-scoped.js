77c90bbfb03ff54fed5838171b3eab69eaf8b72f
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-13 18:42:31 +0100

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-    if (trueOrFalse) {
+
+  if (trueOrFalse) {
+    /* eslint-disable */
       const task = false;
       const task2 = true;
-    }
+}
 
   return [task, task2];
 }
