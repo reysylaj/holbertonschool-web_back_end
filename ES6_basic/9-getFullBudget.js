7212f7bddeb688f43c3e42a361c5431d4ea98330
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-13 18:45:46 +0100

    Update 9-getFullBudget.js

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,16 +1,17 @@
+/*eslint-disable */
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-    const budget = getBudgetObject(income, gdp, capita);
-    const fullBudget = {
-        ...budget,
-        getIncomeInDollars: function (income) {
-            return `$${income}`;
-        },
-        getIncomeInEuros: function (income) {
-            return `${income} euros`;
-        },
-    };
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => {
+      return `$${income}`;
+    },
+    getIncomeInEuros: (income) => {
+      return `${income} euros`;
+    },
+  };
 
-    return fullBudget;
+  return fullBudget;
 }
