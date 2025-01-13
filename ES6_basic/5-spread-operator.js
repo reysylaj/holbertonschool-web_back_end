0cf700eb063bf8ccc665c76dfafd17098a844286
reysylaj <rey.sylaj.20@gmail.com>
2025-01-13 18:23:54 +0100

    edit 5

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-    const values = [[...array1] + " " + [...array2] + "," + [...string]]
-    return values;
+    return [...array1, ...array2, ...string];
 }
