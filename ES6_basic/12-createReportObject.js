cf1d8067ebeb122cbc83874db810a9e3451ce2e8
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-13 18:49:55 +0100

    Update 12-createReportObject.js

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
+
 export default function createReportObject(employeesList) {
-    return {
-        allEmployees: { ...employeesList },  // Spread the employeesList into allEmployees
-        getNumberOfDepartments: function () {  // Method to get the number of departments
-            return Object.keys(this.allEmployees).length;  // Count the number of department keys
-        }
-    };
-}
+  return {
+    allEmployees: employeesList, 
+return Object.keys(employeesList).length; 
+      }
+    }
+  
