86e9066b886dfabab8f1ccef1acb098e3453c2b6
reysylaj <rey.sylaj.20@gmail.com>
2025-01-14 23:31:27 +0100

    task 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    const signUpPromise = signUpUser(firstName, lastName);
+    const uploadPromise = uploadPhoto(fileName);
+
+    return Promise.allSettled([signUpPromise, uploadPromise])
+        .then((results) => {
+            return results.map((result) => ({
+                status: result.status,
+                value: result.status === 'fulfilled' ? result.value : result.reason,  // No need for .message now
+            }));
+        });
+}
