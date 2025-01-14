bb1f35d0ae264566a2cbf733e290444d0de8ed79
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-14 23:23:28 +0100

    Update 0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 export default function getResponseFromApi() {
   return new Promise((resolve, reject) => {
-	resolve('');
-	reject(new Error('Something bad happend'));
+    resolve('');
+    reject(new Error('Something bad happend'));
   });
 }
