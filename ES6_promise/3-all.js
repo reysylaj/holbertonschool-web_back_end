427db4eab4b24925621de136ef7c94176b923bb1
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-15 00:17:28 +0100

    Update 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser(),])
     .then((Response) => {
       console.log(
         `${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`
