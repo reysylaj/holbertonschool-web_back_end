df22b41a685ed037a519bb30f92b5baf8a01c994
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-15 00:05:50 +0100

    Update 9-try.js

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -2,15 +2,15 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-        // Try to execute the mathFunction and push its result to the queue
+    // Try to execute the mathFunction and push its result to the queue
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-        // If an error is thrown, push the full error message to the queue
+    // If an error is thrown, push the full error message to the queue
     queue.push(`Error: ${error.message}`);
   }
 
-    // Always push this message at the end of the queue
+  // Always push this message at the end of the queue
   queue.push('Guardrail was processed');
 
   return queue;
