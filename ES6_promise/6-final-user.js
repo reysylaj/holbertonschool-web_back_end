529b6be42899077ec78014444816c8de21717dfa
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-15 00:44:09 +0100

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,7 +7,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled([signUpPromise, uploadPromise])
     .then((results) => {
-      return results.map((result) => ({
+      results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : result.reason,
       }));
