6651d9de56ac1f47670a2b36174728818f172581
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-14 23:45:00 +0100

    Update 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from "./utils";
 
 function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((Response) => {
-            console.log(
-                `${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`
-            );
-        })
-        .catch(() => console.log("Signup system offline"));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((Response) => {
+      console.log(
+        `${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`
+      );
+    })
+    .catch(() => console.log("Signup system offline"));
 }
 
 export default handleProfileSignup;
