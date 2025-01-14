e0ab7fae2ed3a43069e635a04e50ae0946110f4f
reysylaj <rey.sylaj.20@gmail.com>
2025-01-14 23:18:17 +0100

    edit eslintrc,js

--- ES6_promise/.eslintrc.js
+++ ES6_promise/.eslintrc.js
@@ -1,36 +1,17 @@
+import eslint from 'eslint';
 
-module.exports = {
+const { ESLint } = eslint; // ESLint package
+
+export default {
     env: {
-        browser: false,
+        node: true,
         es6: true,
-        jest: true,
-    },
-    extends: [
-        'airbnb-base',
-        'plugin:jest/all',
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
     },
     parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 2020,
         sourceType: 'module',
     },
-    plugins: ['jest'],
     rules: {
         'no-console': 'off',
-        'no-shadow': 'off',
-        'no-restricted-syntax': [
-            'error',
-            'LabeledStatement',
-            'WithStatement',
-        ],
     },
-    overrides: [
-        {
-            files: ['*.js'],
-            excludedFiles: 'babel.config.js',
-        }
-    ]
 };
