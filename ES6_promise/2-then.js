c721b8e8fb5a5640d886f907b099140d2f2502c5
reysylaj <rey.sylaj.20@gmail.com>
2025-01-14 23:27:28 +0100

    task 2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+function handleResponseFromAPI(promise) {
+    return promise
+        .then(() => {
+            return { status: 200, body: 'success' };
+        })
+        .catch(() => {
+            return new Error();
+        })
+        .finally(() => {
+            console.log('Got a response from the API');
+        });
+}
+
+export default handleResponseFromAPI;
