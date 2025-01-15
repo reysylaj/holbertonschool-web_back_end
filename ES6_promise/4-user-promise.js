1cf7eb1d9579642d72f5ae265b0cf0ea17b78749
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-15 01:07:40 +0100

    Update 4-user-promise.js

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,9 +1,8 @@
 function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-      firstName: firstName,
-      lastName: lastName,
-    });
-  }
-  
-  export default signUpUser;
-  
+  return Promise.resolve({
+    firstName: firstName,
+    lastName: lastName,
+    firstName,
+    lastName,
+  });
+}
