6b1461ba8e5b8236d17d9890418fec7e65ffab13
reysylaj <130251578+reysylaj@users.noreply.github.com>
2025-01-15 01:02:41 +0100

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,10 +6,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) =>
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }))
+    .then((results) => {
+      return results.map((result) => {
+        return {
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : result.reason
+        };
+      });
     });
 }
