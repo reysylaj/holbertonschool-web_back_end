1ae1051a97c1a55bc624921d568e2b279f8c0147
reysylaj <rey.sylaj.20@gmail.com>
2025-01-21 20:48:30 +0100

    Created task 8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
+export default HolbertonClass;
