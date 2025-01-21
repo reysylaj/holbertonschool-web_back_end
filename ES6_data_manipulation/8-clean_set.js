5322ae05498e93eea4aa2d454f0c5f51b0b297ce
reysylaj <rey.sylaj.20@gmail.com>
2025-01-21 21:32:16 +0100

    Edit Task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 export default function cleanSet(set, startString) {
   if (typeof startString !== 'string') {
     return '';
-    }
+  }
 
   if (startString === '') {
     return [...set].join('-');
-    }
+  }
 
   const result = [];
 
-  for (let item of set) {
+  for (const item of set) {
     if (typeof item === 'string' && item.startsWith(startString)) {
       result.push(item.slice(startString.length));
     }
