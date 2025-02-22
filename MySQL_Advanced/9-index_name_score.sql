0eafd7daf1d62ad8e3f238a6cd6f8cdd440c5c4a
reysylaj <rey.sylaj.20@gmail.com>
2025-02-22 15:21:09 +0100

    Edited task 9

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,2 @@
--- Create an index on the first letter of the name and the score
-CREATE INDEX idx_name_first_score
-ON names (LEFT(name, 1), score);
+-- Create an index on the first character of 'name' and 'score'
+CREATE INDEX idx_name_first_score ON names (name(1), score);
